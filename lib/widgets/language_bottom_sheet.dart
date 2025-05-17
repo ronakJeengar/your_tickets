66574dc63b5454ff46c1ac547c70a1f274a684d0
Ronak Jeengar <ronakjeengar7860@gmail.com>
2025-05-17 20:32:48 +0530

    riverpod added

--- lib/widgets/language_bottom_sheet.dart
+++ lib/widgets/language_bottom_sheet.dart
@@ -1,40 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:your_tickets/constants/app_colors.dart';
 import 'package:your_tickets/constants/gap.dart';
+import 'package:your_tickets/providers/dashboard_provider.dart';
 import 'package:your_tickets/widgets/horizontal_divider.dart';
 import 'package:your_tickets/widgets/primary_button.dart';
-import 'radio_with_label.dart';
+import 'package:your_tickets/widgets/radio_with_label.dart';
 
-class LanguageBottomSheet extends StatefulWidget {
-  final String language;
-
-  const LanguageBottomSheet({super.key, required this.language});
+class LanguageBottomSheet extends ConsumerWidget {
+  const LanguageBottomSheet({super.key, required String language});
 
   @override
-  State<LanguageBottomSheet> createState() => _LanguageBottomSheetState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedLanguage = ref.watch(selectedLanguageProvider);
 
-class _LanguageBottomSheetState extends State<LanguageBottomSheet> {
-  String _selectedLanguage = '';
-
-  @override
-  void initState() {
-    _selectedLanguage = widget.language;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(20),
       width: double.infinity,
       decoration: const BoxDecoration(
-          color: AppColors.lightBlackColor,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(15),
-            topLeft: Radius.circular(15),
-          )),
+        color: AppColors.lightBlackColor,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(15),
+          topLeft: Radius.circular(15),
+        ),
+      ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -42,45 +32,40 @@ class _LanguageBottomSheetState extends State<LanguageBottomSheet> {
           const Text(
             'Choose Language',
             style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: AppColors.lightWhiteColor),
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: AppColors.lightWhiteColor,
+            ),
           ),
           const Text(
             'Which language do you want to use?',
             style: TextStyle(fontSize: 14, color: AppColors.lightWhiteColor),
           ),
           gapV20(),
-          RadioWithLabel(
-            label: 'English',
-            value: 'English',
-            groupValue: _selectedLanguage,
-            onChanged: (value) {
-              setState(() {
-                _selectedLanguage = value;
-              });
-            },
-          ),
-          gapV10(),
-          const HorizontalDivider(),
-          gapV10(),
-          RadioWithLabel(
-            label: 'Hindi',
-            value: 'Hindi',
-            groupValue: _selectedLanguage,
-            onChanged: (value) {
-              setState(() {
-                _selectedLanguage = value;
-              });
-            },
+          ...['English', 'Hindi'].map(
+                (lang) => Column(
+              children: [
+                RadioWithLabel(
+                  label: lang,
+                  value: lang,
+                  groupValue: selectedLanguage,
+                  onChanged: (value) {
+                    ref.read(selectedLanguageProvider.notifier).state = value;
+                  },
+                ),
+                if (lang != 'Hindi') const HorizontalDivider(),
+                gapV10(),
+              ],
+            ),
           ),
           const Spacer(),
           PrimaryButton(
-              label: 'Use $_selectedLanguage',
-              onPressed: () {
-                context.pop(_selectedLanguage);
-              },
-              isLoading: false),
+            label: 'Use $selectedLanguage',
+            onPressed: () {
+              context.pop(selectedLanguage);
+            },
+            isLoading: false,
+          ),
         ],
       ),
     );
